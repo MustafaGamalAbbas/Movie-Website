d33c25e90bb17032f079e73ca9eaf6548928cf79
Musatfa Gamal Abbas <Mustafapiso@outlook.com>
2018-05-01 06:05:04 +0200

    enhance the code

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
 import fresh_tomatoes as ft
 from models import Movie
 
-avengers_movie = Movie()
-avengers_movie.movie_title = "Avengers"
-avengers_movie.poster_image_url = "http://image.tmdb.org/t/p/w342/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg"
-avengers_movie.trailer_youtube_url = "https://www.youtube.com/watch?v=6ZfuNTqbHE8"
+avengers_movie = Movie("Avengers",
+                       "http://image.tmdb.org/t/p/w342/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+                       "https://www.youtube.com/watch?v=6ZfuNTqbHE8")
 
-zootopia_movie = Movie()
-zootopia_movie.movie_title = "Zootopia"
-zootopia_movie.poster_image_url = "http://image.tmdb.org/t/p/w342/sM33SANp9z6rXW8Itn7NnG1GOEs.jpg"
-zootopia_movie.trailer_youtube_url = "https://www.youtube.com/watch?v=jWM0ct-OLsM"
+zootopia_movie = Movie("Zootopia",
+                       "http://image.tmdb.org/t/p/w342/sM33SANp9z6rXW8Itn7NnG1GOEs.jpg",
+                       "https://www.youtube.com/watch?v=jWM0ct-OLsM")
 
-coco_movie = Movie()
-coco_movie.movie_title = "Coco"
-coco_movie.poster_image_url = "http://image.tmdb.org/t/p/w342/eKi8dIrr8voobbaGzDpe8w0PVbC.jpg"
-coco_movie.trailer_youtube_url = "https://www.youtube.com/watch?v=Ga6RYejo6Hk"
+coco_movie = Movie("Coco",
+                   "http://image.tmdb.org/t/p/w342/eKi8dIrr8voobbaGzDpe8w0PVbC.jpg",
+                   "https://www.youtube.com/watch?v=Ga6RYejo6Hk")
 
-rampage_moive = Movie()
-rampage_moive.movie_title = "Rampage"
-rampage_moive.poster_image_url = "http://image.tmdb.org/t/p/w342/30oXQKwibh0uANGMs0Sytw3uN22.jpg"
-rampage_moive.trailer_youtube_url = "https://www.youtube.com/watch?v=coOKvrsmQiI"
+rampage_movie = Movie("Rampage",
+                      "http://image.tmdb.org/t/p/w342/30oXQKwibh0uANGMs0Sytw3uN22.jpg",
+                      "https://www.youtube.com/watch?v=coOKvrsmQiI")
 
-movies = (avengers_movie, zootopia_movie, coco_movie, rampage_moive)
-ft.open_movies_page(movies)
+movies_list = [avengers_movie, zootopia_movie, coco_movie, rampage_movie]
+
+ft.open_movies_page(movies_list)
--- models.py
+++ models.py
@@ -1,6 +1,11 @@
-
 class Movie:
-    def __init__(self):
-        self.title = ""
-        self.poster_image_url = ""
-        self.trailer_youtube_url = ""
+    def __init__(self, title, poster_image_url, trailer_youtube_url):
+        """
+         Initiate an instance of a movie object
+        :param title: String
+        :param poster_image_url:String
+        :param trailer_youtube_url: String
+        """
+        self.title = title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
